f745d5848bcd1ca39411771c85bc1950bf255630
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2021-04-10 11:03:20 +0300

    Add files via upload

---
+++ CarController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float movementSpeed = 15;
+    public float rotationSpeed = 4;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + transform.forward * movementSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        float turn = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+}
---
+++ SetupLocalPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            gameObject.GetComponent<CarController>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<CarController>().enabled = false;
+        }
+
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        Camera.main.GetComponent<CameraFollow>().setTarget(gameObject.transform);
+    }
+}
